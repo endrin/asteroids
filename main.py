9eb22af8bdb148079c619bb379ea18bfae082730
endrin <158015+endrin@users.noreply.github.com>
2025-01-13 19:46:25 +0200

    Draw the main screen
    Empty so far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import display
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
@@ -7,6 +8,19 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+
+        display.flip()
 
 
 if __name__ == "__main__":
