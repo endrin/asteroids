da124f04867f5f93c5275b4015e2713054a09e56
endrin <158015+endrin@users.noreply.github.com>
2025-01-18 22:27:03 +0200

    Make player hitbox visible

--- player.py
+++ player.py
@@ -19,6 +19,7 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.circle(screen, "firebrick", self.position, self.radius, 1)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
